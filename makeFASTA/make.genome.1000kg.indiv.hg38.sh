c3aeb0ad9ff7d11d6f87486c70fa126af6c366da
Erin <erin.gilbertson@ucsf.edu>
2021-10-20 14:21:29 -0700

    fixed ind chrm files

--- makeFASTA/make.genome.1000kg.indiv.hg38.sh
+++ makeFASTA/make.genome.1000kg.indiv.hg38.sh
@@ -36,8 +36,9 @@ if ! [ -s tmp${INDIV}.${CHR} ]; then
 	exit
 fi
 
-IDX=$(sed -n $'1s/\t/\\\n/gp' tmp${INDIV}.${CHR} | grep -nx $INDIV | cut -d: -f1)
 
+IDX=$(sed -n $'1s/\t/\\\n/gp' tmp${INDIV}.${CHR} | grep -nx $INDIV | cut -d: -f1)
+rm tmp${INDIV}.${CHR}
 ### make new vcf file considering only SNPs, and excluding tri-allelic sites
 
 zcat $INPUTVCF | awk -F '\t|:' -v IDV="$IDX" '/^[^#]/ {if($IDV!="0|0" && $IDV!="2|2" && $IDV!="0|2" && $IDV!="2|0" && $8~/VT=SNP/) print "chr"$1, $2, $3, $4, $5, ".", "." ,$IDV}' OFS='\t' > tmp${CHR}${INDIV}.vcf
@@ -52,20 +53,20 @@ echo "made new vcf"
 ### build new genome fasta
 
 gatk FastaAlternateReferenceMaker\
- -R /wynton/group/capra/data/wynton_databases/goldenPath/hg38/chromosomes/chr${CHR}.fa.gz\
+ -R /wynton/home/capra/egilbertson/data/human_genome/chrms/chr${CHR}.fa\
  -V chr${CHR}_${INDIV}.vcf.gz\
- -O chr${CHR}_${INDIV}_hg19_full.fa
+ -O chr${CHR}_${INDIV}_hg38_full.fa
 
 echo "build fasta genome"
 ### Fix GATK output's default fasta headers
-sed -i "s/>1/>chr$CHR/g" chr${CHR}_${INDIV}_hg19_full.fa
+sed -i "s/>1/>chr$CHR/g" chr${CHR}_${INDIV}_hg38_full.fa
 echo "fix fasta headers"
 ### remove old dict and index file and remake using the corrected fasta header
 
-rm chr${CHR}_${INDIV}_hg19_full.dict
-rm chr${CHR}_${INDIV}_hg19_full.fa.fai
+rm chr${CHR}_${INDIV}_hg38_full.dict
+rm chr${CHR}_${INDIV}_hg38_full.fa.fai
 
-gatk CreateSequenceDictionary -R chr${CHR}_${INDIV}_hg19_full.fa
-samtools faidx chr${CHR}_${INDIV}_hg19_full.fa
+gatk CreateSequenceDictionary -R chr${CHR}_${INDIV}_hg38_full.fa
+samtools faidx chr${CHR}_${INDIV}_hg38_full.fa
 echo "done"
 ###
