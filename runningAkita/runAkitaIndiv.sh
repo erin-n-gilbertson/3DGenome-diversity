f65584a3646cbdac2e67cf6224dd97767080a473
Erin <erin.gilbertson@ucsf.edu>
2021-10-26 14:12:11 -0700

    .

--- runningAkita/runAkitaIndiv.sh
+++ runningAkita/runAkitaIndiv.sh
@@ -4,19 +4,19 @@
 #$ -m a
 #$ -l h_rt=55:00:00
 #$ -l mem_free=10G
-#$ -o /wynton/group/capra/projects/modern_human_3Dgenome/stdout/runAkitaOneIndiv_%a.out
+#$ -o /wynton/group/capra/projects/modern_human_3Dgenome/stdout/runAkitaIndiv/
+#$ -e /wynton/group/capra/projects/modern_human_3Dgenome/stdout/runAkitaIndiv/
 #$ -t 1
-#$ -cwd
 #!/usr/bin/env python
 
 echo "JOB_NAME: ${JOB_NAME}"
 echo "JOBID:  ${JOB_ID}"
 
 
-source activate akita
+conda activate akita
 
 #Identity individual to run Akita on using the listOfIndivs.txt file and array taskid
-indiv=$(awk -v var="$$SGE_TASK_ID" 'NR==var' listOfIndivs_all.txt)
+indiv=$(awk -v var="$SGE_TASK_ID" 'NR==var' /wynton/group/capra/projects/modern_human_3Dgenome/data/listOfIndivs_all.txt)
 echo "Indiv: ${indiv}"
 #python runAkitaOnOneIndiv_noHarmonization.py "$indiv"
 python runAkitaOnOneIndiv_noHarmonization.py "$indiv"
