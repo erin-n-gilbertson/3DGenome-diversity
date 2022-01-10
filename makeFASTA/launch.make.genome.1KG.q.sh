69b88bb56fd7e7423226e0416bcbbadf1166185e
Erin <erin.gilbertson@ucsf.edu>
2022-01-10 13:56:45 -0800

    shorter arrays

--- makeFASTA/launch.make.genome.1KG.q.sh
+++ makeFASTA/launch.make.genome.1KG.q.sh
@@ -14,9 +14,9 @@ echo $SGE_TASK_ID
 INDIV=$(awk -v var="$SGE_TASK_ID" 'NR==var' /wynton/group/capra/projects/modern_human_3Dgenome/data/1kg_indivs_fix_eas_female.txt)
 echo $INDIV
 
-
-qsub -N make.genome.$INDIV -v "INDIV=$INDIV" -l mem_free=40G -t 1-4 -l h_rt=2:00:00 /wynton/group/capra/projects/modern_human_3Dgenome/bin/makeFASTA/make.genome.1KG.indiv.q
-qsub -N make.genome.$INDIV -v "INDIV=$INDIV" -l mem_free=19G -t 5-23 -l h_rt=2:00:00 /wynton/group/capra/projects/modern_human_3Dgenome/bin/makeFASTA/make.genome.1KG.indiv.q
-
+qsub -N make.genome.$INDIV -v "INDIV=$INDIV" -l mem_free=40G -t 1 -l h_rt=2:00:00 /wynton/group/capra/projects/modern_human_3Dgenome/bin/makeFASTA/make.genome.1KG.indiv.q
+# qsub -N make.genome.$INDIV -v "INDIV=$INDIV" -l mem_free=40G -t 1-4 -l h_rt=2:00:00 /wynton/group/capra/projects/modern_human_3Dgenome/bin/makeFASTA/make.genome.1KG.indiv.q
+# qsub -N make.genome.$INDIV -v "INDIV=$INDIV" -l mem_free=19G -t 5-23 -l h_rt=2:00:00 /wynton/group/capra/projects/modern_human_3Dgenome/bin/makeFASTA/make.genome.1KG.indiv.q
+#
 
 #
