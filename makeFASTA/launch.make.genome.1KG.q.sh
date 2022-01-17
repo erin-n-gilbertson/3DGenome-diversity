1ab54b7e824b858013d0dca497f2d8ce08917671
Erin <erin.gilbertson@ucsf.edu>
2022-01-17 10:21:31 -0800

    Do it for everyone

--- makeFASTA/launch.make.genome.1KG.q.sh
+++ makeFASTA/launch.make.genome.1KG.q.sh
@@ -2,7 +2,7 @@
 #$ -m a
 #$ -o /wynton/group/capra/projects/modern_human_3Dgenome/stdout/make_genome/launch.o
 #$ -e /wynton/group/capra/projects/modern_human_3Dgenome/stdout/make_genome/launch.e
-#$ -t 1
+#$ -t 1-410
 
 ## the called SLURM script usees an internal array (ARRY) of chromosome names that the slurm array IDs are then used to index
 ## ARRY=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X)
