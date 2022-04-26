18bf6742e709f90a776a9e2cc5bb3deeea632b86
erin <erin.gilbertson@ucsf.edu>
2022-04-26 10:16:16 -0700

    fix job path

--- makeFASTA/RUN_launch.make.genome.1KG.q.sh
+++ makeFASTA/RUN_launch.make.genome.1KG.q.sh
@@ -4,11 +4,11 @@
 #$ -e /wynton/group/capra/projects/modern_human_3Dgenome/stdout/make_genome/launch.e
 
 
-qsub launch.make.genome.1KG_afr.q.sh
-qsub launch.make.genome.1KG_amr.q.sh
-qsub launch.make.genome.1KG_eas.q.sh
-qsub launch.make.genome.1KG_eur.q.sh
-qsub launch.make.genome.1KG_sas.q.sh
+qsub /wynton/group/capra/projects/modern_human_3Dgenome/bin/makeFASTA/launch.make.genome.1KG_afr.q.sh
+qsub /wynton/group/capra/projects/modern_human_3Dgenome/bin/makeFASTA/launch.make.genome.1KG_amr.q.sh
+qsub /wynton/group/capra/projects/modern_human_3Dgenome/bin/makeFASTA/launch.make.genome.1KG_eas.q.sh
+qsub /wynton/group/capra/projects/modern_human_3Dgenome/bin/makeFASTA/launch.make.genome.1KG_eur.q.sh
+qsub /wynton/group/capra/projects/modern_human_3Dgenome/bin/makeFASTA/launch.make.genome.1KG_sas.q.sh
 
 
 #
