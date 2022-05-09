a25a5c0ac400ca25b5740be0eae4fb12bc5b552d
erin <erin.gilbertson@ucsf.edu>
2022-05-09 14:16:21 -0700

    comment

--- makeFASTA/make.genome.1KG.one_chromosome.sh
+++ makeFASTA/make.genome.1KG.one_chromosome.sh
@@ -52,48 +52,48 @@ echo "DIR: ${LOC}"
 cd $LOC
 pwd
 ### Find column index for individual
-
-rm tmp${INDIV}.${CHR}
-zcat $INPUTVCF | grep -m 1 $INDIV > tmp${INDIV}.${CHR}
-
-if ! [ -s tmp${INDIV}.${CHR} ]; then
-	echo "Individual "${INDIV}" not found on chromosome "${CHR}"in 1000 genomes vcf"
-	exit
-fi
-
-
-IDX=$(sed -n $'1s/\t/\\\n/gp' tmp${INDIV}.${CHR} | grep -nx $INDIV | cut -d: -f1)
-rm tmp${INDIV}.${CHR}
-### make new vcf file considering only SNPs, and excluding tri-allelic sites
-
-zcat $INPUTVCF | awk -F '\t|:' -v IDV="$IDX" '/^[^#]/ {if($IDV!="0|0" && $IDV!="2|2" && $IDV!="0|2" && $IDV!="2|0" && $8~/VT=SNP/) print "chr"$1, $2, $3, $4, $5, ".", "." ,$IDV}' OFS='\t' > tmp${CHR}${INDIV}.vcf
-
-cat ${VCFHEAD} tmp${CHR}${INDIV}.vcf > chr${CHR}_${INDIV}.vcf
-rm tmp${CHR}${INDIV}.vcf
-bgzip -c chr${CHR}_${INDIV}.vcf > chr${CHR}_${INDIV}.vcf.gz
-tabix -p vcf chr${CHR}_${INDIV}.vcf.gz
-rm chr${CHR}_${INDIV}.vcf
-
-echo "made new vcf"
-### build new genome fasta
-echo "REFCHRDIR: ${REFCHRDIR}"
-echo "CHR: ${CHR}"
-echo "INDIV: ${INDIV}"
-gatk FastaAlternateReferenceMaker\
- -R $REFCHRDIR/chr${CHR}.fa\
- -V chr${CHR}_${INDIV}.vcf.gz\
- -O chr${CHR}_${INDIV}_hg38_full.fa
-
-echo "build fasta genome"
-### Fix GATK output's default fasta headers
-sed -i "s/>1/>chr$CHR/g" chr${CHR}_${INDIV}_hg38_full.fa
-echo "fix fasta headers"
-### remove old dict and index file and remake using the corrected fasta header
-
-rm chr${CHR}_${INDIV}_hg38_full.dict
-rm chr${CHR}_${INDIV}_hg38_full.fa.fai
-
-gatk CreateSequenceDictionary -R chr${CHR}_${INDIV}_hg38_full.fa
-samtools faidx chr${CHR}_${INDIV}_hg38_full.fa
-echo "done"
-###
+#
+# rm tmp${INDIV}.${CHR}
+# zcat $INPUTVCF | grep -m 1 $INDIV > tmp${INDIV}.${CHR}
+#
+# if ! [ -s tmp${INDIV}.${CHR} ]; then
+# 	echo "Individual "${INDIV}" not found on chromosome "${CHR}"in 1000 genomes vcf"
+# 	exit
+# fi
+#
+#
+# IDX=$(sed -n $'1s/\t/\\\n/gp' tmp${INDIV}.${CHR} | grep -nx $INDIV | cut -d: -f1)
+# rm tmp${INDIV}.${CHR}
+# ### make new vcf file considering only SNPs, and excluding tri-allelic sites
+#
+# zcat $INPUTVCF | awk -F '\t|:' -v IDV="$IDX" '/^[^#]/ {if($IDV!="0|0" && $IDV!="2|2" && $IDV!="0|2" && $IDV!="2|0" && $8~/VT=SNP/) print "chr"$1, $2, $3, $4, $5, ".", "." ,$IDV}' OFS='\t' > tmp${CHR}${INDIV}.vcf
+#
+# cat ${VCFHEAD} tmp${CHR}${INDIV}.vcf > chr${CHR}_${INDIV}.vcf
+# rm tmp${CHR}${INDIV}.vcf
+# bgzip -c chr${CHR}_${INDIV}.vcf > chr${CHR}_${INDIV}.vcf.gz
+# tabix -p vcf chr${CHR}_${INDIV}.vcf.gz
+# rm chr${CHR}_${INDIV}.vcf
+#
+# echo "made new vcf"
+# ### build new genome fasta
+# echo "REFCHRDIR: ${REFCHRDIR}"
+# echo "CHR: ${CHR}"
+# echo "INDIV: ${INDIV}"
+# gatk FastaAlternateReferenceMaker\
+#  -R $REFCHRDIR/chr${CHR}.fa\
+#  -V chr${CHR}_${INDIV}.vcf.gz\
+#  -O chr${CHR}_${INDIV}_hg38_full.fa
+#
+# echo "build fasta genome"
+# ### Fix GATK output's default fasta headers
+# sed -i "s/>1/>chr$CHR/g" chr${CHR}_${INDIV}_hg38_full.fa
+# echo "fix fasta headers"
+# ### remove old dict and index file and remake using the corrected fasta header
+#
+# rm chr${CHR}_${INDIV}_hg38_full.dict
+# rm chr${CHR}_${INDIV}_hg38_full.fa.fai
+#
+# gatk CreateSequenceDictionary -R chr${CHR}_${INDIV}_hg38_full.fa
+# samtools faidx chr${CHR}_${INDIV}_hg38_full.fa
+# echo "done"
+# ###
