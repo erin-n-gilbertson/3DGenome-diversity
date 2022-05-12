1cd556d33209f86924ca0f45279026c160dc25a2
erin <erin.gilbertson@ucsf.edu>
2022-05-12 15:00:22 -0700

    correct config

--- runningAkita/runAkita.sh
+++ runningAkita/runAkita.sh
@@ -19,7 +19,7 @@ source ~/.bashrc
 source /wynton/home/capra/egilbertson/envs/akita/bin/activate
 
 source ~/bin/bash_utils/ini_parse
-CONFIGPATH='/wynton/group/capra/projects/modern_human_3Dgenome/bin/activeNotebooks/config_makeFASTA_1KG_SNVs.ini'
+CONFIGPATH='/wynton/group/capra/projects/modern_human_3Dgenome/bin/activeNotebooks/config_runAkita.ini'
 
 INDIV_LIST=$(cat ${CONFIGPATH} | getSetting 'PATH' 'IND_LIST')
 echo ${INDIV_LIST}
