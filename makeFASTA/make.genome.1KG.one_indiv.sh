2147f2cd573bc5df7cdfc67f13624c8aaf3eacb4
erin <erin.gilbertson@ucsf.edu>
2022-05-12 09:50:37 -0700

    hopefully working bash config!!

--- makeFASTA/make.genome.1KG.one_indiv.sh
+++ makeFASTA/make.genome.1KG.one_indiv.sh
@@ -13,35 +13,40 @@ echo "Indiv: ${INDIV}"
 
 
 
+
+
+## path to config.ini file that specifies variables and parameters needed here.
+## THIS IS THE ONLY THING THAT SHOULD HAVE TO CHANGE!!!
+
+CONFIGPATH='/wynton/group/capra/projects/modern_human_3Dgenome/bin/activeNotebooks/config_makeFASTA_1KG_SNVs.ini'
+echo "configpath: ${CONFIGPATH}"
+
+VCFPTH=$(cat ${CONFIGPATH} | getSetting 'PATH' 'vcf_path')
+
+
+PFX=$(cat ${CONFIGPATH} | getSetting 'PARAMS' 'vcf_pfx')
+SFX=$(cat ${CONFIGPATH} | getSetting 'PARAMS' 'vcf_sfx')
+
+CHRPATH=$(cat ${CONFIGPATH} | getSetting 'PATH' 'chr_path')
+VCFHEAD=$(cat ${CONFIGPATH} | getSetting 'PATH' 'vcf_head')
+REFCHRDIR=$(cat ${CONFIGPATH} | getSetting 'PATH' 'ref_path')
+
+
 ARRY=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X)
 
 
 CHR=${ARRY[$SGE_TASK_ID-1]}
 echo "chr.q: ${CHR}"
 
-## path to config.ini file that specifies variables and parameters needed here.
-CONFIGPATH='/wynton/group/capra/projects/modern_human_3Dgenome/bin/activeNotebooks/config_makeFASTA_1KG_SNVs.ini'
-echo "configpath: ${CONFIGPATH}"
+cd /wynton/group/capra/data/wynton_databases/1000_genomes/release/20130502
+DIRNAME=$(grep ${INDIV} integrated_call_samples_v3.20130502.ALL.panel | awk '{print $3"_"$2"_"$4"_"$1}')
+echo "Directory: ${DIRNAME}"
 
-VCFPTH=$(cat ${CONFIGPATH} | getSetting 'PATH' 'vcf_path')
+WRKDIR=$(cat ${CONFIGPATH} | getSetting 'PATH' 'fasta_out_path')
+cd ${WRKDIR}
 
-echo "VCFPTH: ${VCFPTH}"
-# PFX='ALL.chr'
-# SFX='.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz'
-#
-# CHRPATH='/wynton/group/capra/data/wynton_databases/goldenPath/hg38/bigZips/latest/hg38.chrom.sizes'
-# VCFHEAD='/wynton/home/capra/egilbertson/data/vcf.header.txt'
-# REFCHRDIR='/wynton/home/capra/egilbertson/data/human_genome/chrms'
-#
-# cd /wynton/group/capra/data/wynton_databases/1000_genomes/release/20130502
-# DIRNAME=$(grep ${INDIV} integrated_call_samples_v3.20130502.ALL.panel | awk '{print $3"_"$2"_"$4"_"$1}')
-# echo "Directory: ${DIRNAME}"
-#
-# WRKDIR='/wynton/group/capra/projects/modern_human_3Dgenome/data/genomes'
-# cd ${WRKDIR}
-#
-# mkdir -p "${DIRNAME}"
-# cd ${DIRNAME}
-#
-#
-# bash /wynton/group/capra/projects/modern_human_3Dgenome/bin/makeFASTA/make.genome.1KG.one_chromosome.sh ${CHR} ${INDIV} ${VCFPTH} ${PFX} ${SFX} ${DIRNAME} ${CHRPATH} ${WRKDIR} ${VCFHEAD} ${REFCHRDIR}
+mkdir -p "${DIRNAME}"
+cd ${DIRNAME}
+
+
+bash ${$(cat ${CONFIGPATH} | getSetting 'BIN' 'make_fasta_chrm')} ${CHR} ${INDIV} ${VCFPTH} ${PFX} ${SFX} ${DIRNAME} ${CHRPATH} ${WRKDIR} ${VCFHEAD} ${REFCHRDIR}
