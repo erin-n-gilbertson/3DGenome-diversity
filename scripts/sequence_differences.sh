3608a473a8db69884e00c6cdd763dc0faf691154
erin <erin.gilbertson@ucsf.edu>
2022-08-29 12:27:52 -0500

    ohohwoah gotta make the first time last

--- scripts/sequence_differences.sh
+++ scripts/sequence_differences.sh
@@ -20,8 +20,8 @@ source /wynton/home/capra/egilbertson/envs/akita/bin/activate
 cd /wynton/group/capra/projects/modern_human_3Dgenome/data/pairwise/sequence
 
 # assign variables using the SGE task ID to get a specific pair of individuals
-ind1=$(awk -v row=$SGE_TASK_ID 'NR == row {print $1}' /wynton/group/capra/projects/modern_human_3Dgenome/data/reference/baseline_sequence_pairs.txt)
-ind2=$(awk -v row=$SGE_TASK_ID 'NR == row {print $2}' /wynton/group/capra/projects/modern_human_3Dgenome/data/reference/baseline_sequence_pairs.txt)
+ind1=$(awk -v row=$SGE_TASK_ID 'NR == row {print $1}' /wynton/group/capra/projects/modern_human_3Dgenome/data/reference/lists/baseline_sequence_pairs.txt)
+ind2=$(awk -v row=$SGE_TASK_ID 'NR == row {print $2}' /wynton/group/capra/projects/modern_human_3Dgenome/data/reference/lists/baseline_sequence_pairs.txt)
 
 # run
 python /wynton/group/capra/projects/modern_human_3Dgenome/bin/scripts/sequence_differences.py --intervals /wynton/group/capra/projects/modern_human_3Dgenome/data/reference/genome_chunks_large.txt --sample_1 /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/baselines/"$ind1".fa --sample_2 /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/baselines/"$ind2".fa --sample_1_id "$ind1" --sample_2_id "$ind2"
