80a52d4f6317caa88f74e868c217d9b0b87b8519
egilbertson-ucsf <erin.gilbertson@ucsf.edu>
2023-09-06 15:42:35 -0700

    bwa mem

--- HiC_to_Akita/fastq_to_pairs.sh
+++ HiC_to_Akita/fastq_to_pairs.sh
@@ -14,10 +14,10 @@ cd $outdir
 mkdir QC_R1
 mkdir QC_R2
 
-# fastqc -t $nThreads $fastq1 -o $outdir/QC_R1
-# fastqc -t $nThreads $fastq2 -o $outdir/QC_R2
+fastqc -t $nThreads $fastq1 -o $outdir/QC_R1
+fastqc -t $nThreads $fastq2 -o $outdir/QC_R2
 bwa index /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38_reference.fa
-#bwa mem -t $nThreads -SP5M /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38_reference.fa $fastq1 $fastq2 | samtools view -Shb - > $prefix.bam
+bwa mem -t $nThreads -SP5M /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38_reference.fa $fastq1 $fastq2 | samtools view -Shb - > $prefix.bam
 # samtools view -h $prefix.bam | {
 # 	pairtools parse -c /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38.chrom.bed --add-columns mapq
 # } | {
