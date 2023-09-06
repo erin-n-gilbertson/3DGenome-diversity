c7d64603340f83516b0ee0568814d5d51f4148e4
egilbertson-ucsf <erin.gilbertson@ucsf.edu>
2023-09-06 15:46:20 -0700

    fastq to pairs

--- HiC_to_Akita/fastq_to_pairs.sh
+++ HiC_to_Akita/fastq_to_pairs.sh
@@ -18,16 +18,16 @@ fastqc -t $nThreads $fastq1 -o $outdir/QC_R1
 fastqc -t $nThreads $fastq2 -o $outdir/QC_R2
 #bwa index /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38_reference.fa
 bwa mem -t $nThreads -SP5M /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38_reference.fa $fastq1 $fastq2 | samtools view -Shb - > $prefix.bam
-# samtools view -h $prefix.bam | {
-# 	pairtools parse -c /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38.chrom.bed --add-columns mapq
-# } | {
-# 	pairtools sort --nproc $nThreads --memory 32G --compress-program lz4c --tmpdir $outdir --output $prefix.sam.pairs.gz
-# }
-# pairtools dedup --mark-dups --output-dups - --output-unmapped - --output $prefix.marked.sam.pairs.gz $prefix.sam.pairs.gz
-# pairix $prefix.marked.sam.pairs.gz
-# pairtools stats --cmd-in 'pbgzip -dc -n '$nThreads'' -o $prefix.marked.pairs.stats $prefix.marked.sam.pairs.gz
-# pairtools select '(pair_type == "UU") or (pair_type == "UR") or (pair_type == "RU")' --output-rest $prefix.unmapped.sam.pairs.gz --output temp.gz $prefix.marked.sam.pairs.gz
-# pairtools split --output-pairs temp1.gz temp.gz
-# pairtools select 'True' --chrom-subset /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38.chrom.bed -o $prefix.dedup.pairs.gz temp1.gz
-# pairix $prefix.dedup.pairs.gz
+samtools view -h $prefix.bam | {
+	pairtools parse -c /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38.chrom.bed --add-columns mapq
+} | {
+	pairtools sort --nproc $nThreads --memory 32G --compress-program lz4c --tmpdir $outdir --output $prefix.sam.pairs.gz
+}
+pairtools dedup --mark-dups --output-dups - --output-unmapped - --output $prefix.marked.sam.pairs.gz $prefix.sam.pairs.gz
+pairix $prefix.marked.sam.pairs.gz
+pairtools stats --cmd-in 'pbgzip -dc -n '$nThreads'' -o $prefix.marked.pairs.stats $prefix.marked.sam.pairs.gz
+pairtools select '(pair_type == "UU") or (pair_type == "UR") or (pair_type == "RU")' --output-rest $prefix.unmapped.sam.pairs.gz --output temp.gz $prefix.marked.sam.pairs.gz
+pairtools split --output-pairs temp1.gz temp.gz
+pairtools select 'True' --chrom-subset /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38.chrom.bed -o $prefix.dedup.pairs.gz temp1.gz
+pairix $prefix.dedup.pairs.gz
 
