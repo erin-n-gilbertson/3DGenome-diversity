08589885fb7e72045a0d6e004633264aa593e18a
egilbertson-ucsf <erin.gilbertson@ucsf.edu>
2023-09-19 12:25:28 -0700

    update chrom files

--- HiC_to_Akita/fastq_to_pairs.sh
+++ HiC_to_Akita/fastq_to_pairs.sh
@@ -20,7 +20,7 @@ fastqc -t $nThreads $fastq2 -o $outdir/QC_R2
 
 bwa mem -t $nThreads -SP5M /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38_reference.fa $fastq1 $fastq2 | samtools view -Shb - > $prefix.bam
 samtools view -h $prefix.bam | {
-	pairtools parse -c /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38.chrom.bed --add-columns mapq
+	pairtools parse -c /wynton/group/capra/projects/modern_human_3Dgenome/data/experimental/hg38.chromsizes --add-columns mapq
 } | {
 	pairtools sort --nproc $nThreads --memory 32G --compress-program lz4c --tmpdir $outdir --output $prefix.sam.pairs.gz
 }
@@ -29,6 +29,6 @@ pairix $prefix.marked.sam.pairs.gz
 pairtools stats --cmd-in 'pbgzip -dc -n '$nThreads'' -o $prefix.marked.pairs.stats $prefix.marked.sam.pairs.gz
 pairtools select '(pair_type == "UU") or (pair_type == "UR") or (pair_type == "RU")' --output-rest $prefix.unmapped.sam.pairs.gz --output temp.gz $prefix.marked.sam.pairs.gz
 pairtools split --output-pairs temp1.gz temp.gz
-pairtools select 'True' --chrom-subset /wynton/group/capra/projects/modern_human_3Dgenome/data/genomes/hg38.chrom.bed -o $prefix.dedup.pairs.gz temp1.gz
+pairtools select 'True' --chrom-subset /wynton/group/capra/projects/modern_human_3Dgenome/data/experimental/hg38.chromsizes -o $prefix.dedup.pairs.gz temp1.gz
 pairix $prefix.dedup.pairs.gz
 
