cf62c6b8231df7b5ea78258e6b0afa2d4fedf1ab
egilbertson-ucsf <erin.gilbertson@ucsf.edu>
2024-05-07 13:04:17 -0700

    pairwise_all

---
+++ pairwise_all/pairwise_all.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
+
+import scipy.cluster.hierarchy as sch
+from scipy.spatial.distance import pdist
+
+from scipy.spatial.distance import squareform
+from scipy.spatial.distance import pdist, jaccard
+import scipy.spatial as sp, scipy.cluster.hierarchy as hc
+from matplotlib.patches import Rectangle
+from scipy.cluster.hierarchy import to_tree, ClusterNode, dendrogram
+from typing import Dict, Tuple, List, Union, Optional
+import os
+
+from itertools import combinations
+#local
+BASE_PATH = "/".join(os.getcwd().split("/")) # base directory level
+
+# DATA_PATH = os.path.join(BASE_PATH, "../../../downloads/")  # where I dump new data
+# COMP_PATH = os.path.join(DATA_PATH,"1KGvs1KG")
+
+
+#Wynton
+BIN_PATH = os.path.join(BASE_PATH, "bin")  # where my scripts live
+DATA_PATH = os.path.join(BASE_PATH, "data")  # where I dump new data 
+RESULTS_PATH = os.path.join(BASE_PATH, "results")  # where I analyze results
+
+SRC_PATH = os.path.join(BASE_PATH, "src")  # where any packages needed to run analyses live. I haven't started structuring things this way yet. 
+
+COMP_PATH = os.path.join(DATA_PATH, "pairwise/all_windows")
+# COMP_PATH = os.path.join(DATA_PATH,"pairwise/hsmrca")
+# COMP_PATH = os.path.join(DATA_PATH,"pairwise/reference")
+
+
+
+# Create pickle data structures 3D by window
+windows = pd.read_table('%s/intermediates/windows_to_keep.csv' % DATA_PATH, sep=',', index_col=[1,2]).drop(columns=['Unnamed: 0'])
+indivs = pd.read_csv('/wynton/group/capra/projects/modern_human_3Dgenome/data/reference/1KG_unrelated_indivs.txt', index_col=0)
+comp_list = list(combinations(list(indivs['1KG']), 2))
+count = 0
+dict_3d = {} 
+matrix_3d = pd.DataFrame(df[['indiv1','indiv2']])
+for w in list(windows.index):
+    try:
+        df = pd.read_table('%s/%s_%s_comparisons_dict.txt' % (COMP_PATH, w[0], w[1]))
+        df.columns = ['indiv1','indiv2',w]
+        matrix_3d = pd.concat([matrix_3d, df[w]],axis=1)
+        count +=1 
+
+    except:
+        print("error on %s" % w)
+
+    if count%50 == 0:
+        print(count)
+    triu = df.pivot(index='indiv1', columns='indiv2', values='divergence')
+    tril = df.pivot(index='indiv2', columns='indiv1', values='divergence')
+    sym = triu.fillna(tril)
+    new_col = sym.loc['AFR_ACB_female_HG01880'].T
+    new_col = pd.concat([pd.Series([np.nan], index=[new_col.name]), new_col])
+    new_row = sym['SAS_STU_male_HG04229'].T
+    new_row = pd.concat([new_row, pd.Series([np.nan], index=[new_row.name])])
+    sym['AFR_ACB_female_HG01880'] = np.nan
+    sym = pd.concat([sym['AFR_ACB_female_HG01880'], sym[sym.columns[:-1]]], axis=1)
+    sym.loc['SAS_STU_male_HG04229'] = new_row.values
+    sym['AFR_ACB_female_HG01880'] = new_col.values
+    dict_3d[w] = sym
+
+    pickle.dump( dict_3d, open( "dict_3d_all_pairs.p", "wb" ) )
+    pickle.dump( matrix_3d, open( "matrix_3d_all_pairs.p", "wb" ) )
+
+
---
+++ pairwise_all/pairwise_all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#$ -N pairwise_all
+#$ -M erin.gilbertson@ucsf.edu
+#$ -m ae
+#$ -cwd
+#$ -o /wynton/group/capra/projects/modern_human_3Dgenome/stdout/pairwise_all.out
+#$ -e /wynton/group/capra/projects/modern_human_3Dgenome/stdout/pairwise_all.err
+#$ -l h_rt=144:00:00
+#$ -l mem_free=100G
+
+
+# load conda environment
+echo "JOB_NAME: ${JOB_NAME}"
+echo "JOBID:  ${JOB_ID}"
+
+source ~/.bash_profile
+source ~/.bashrc
+#source /wynton/home/capra/egilbertson/envs/akita/bin/activate
+
+load_conda
+conda activate modern3d
+
+
+cd /wynton/group/capra/projects/modern_human_3Dgenome/bin/pairwise_all
+
+
+python3 pairwise_all.py 
